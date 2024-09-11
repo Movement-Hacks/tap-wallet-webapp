0bab406b97694b6cd952d0f5ce7edaf9e84e1e68
starci183 <cuongnvtse160875@gmail.com>
2024-09-11 23:56:28 +0700

    123123

--- src/features/aptos/constants.aptos.ts
+++ src/features/aptos/constants.aptos.ts
@@ -1,7 +1,7 @@
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk"
 
 export const getAptos = (network: Network = Network.DEVNET) =>
-    new Aptos(new AptosConfig({ network }))
+    new Aptos(new AptosConfig({ fullnode: "https://aptos.testnet.suzuka.movementlabs.xyz/v1" }))
 
 export type CoinType = `${string}::${string}::${string}`
 export type AddressType = `0x${string}`
